261775a7ae0ece59055434ce8d3fd93a614689b5
niko <me@nikolasgoebel.com>
2016-12-31 15:35:41 +0100

    Fix latency plot script

--- plot-tpcc.R
+++ plot-tpcc.R
@@ -1,21 +1,16 @@
 
 tpccPlot <- function(name) {
-    xMax <- 10
-    yMax <- 350000
-
     data <- read.csv(file=paste(name, ".csv", sep=""), sep=",", head=TRUE)
 
-    pdf(paste("out/", name, ".pdf", sep=""))
+    pdf(paste("out/", name, "-latency", ".pdf", sep=""))
     
     dt <- (data$end - data$start)
-    dt <- dt[seq(2, length(dt))]
-    # dt <- dt[seq(2, 17303, 1)]
+    dt <- dt[seq(2, length(dt), length.out=50000)]
     g_range <- range(0, dt)
 
-    plot(dt, type="o", axes=FALSE, ann=FALSE, ylim=g_range)
+    plot(dt, type="p", axes=FALSE, ann=FALSE, ylim=g_range)
 
-    axis(1, at=1000*0:length(dt))
-    axis(2, las=1, at=50*0:g_range[2])
+    axis(2, las=1, at=0:g_range[2])
 
     dev.off()
 }
